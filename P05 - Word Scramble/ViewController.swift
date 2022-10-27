0bb27694e093ed45ddb50ceda40ad753fb90bcff
PEDRO HENRIQUE DE OLIVEIRA MORAES <86898701+PedroMoraes89@users.noreply.github.com>
2022-10-27 10:16:35 -0700

    Initial Commit

---
+++ P05 - Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P05 - Word Scramble
+//
+//  Created by user220265 on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
